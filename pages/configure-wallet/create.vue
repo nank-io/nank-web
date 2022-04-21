77bd4faa223ee1f8326ac5560ff421423db549e8
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-04-21 14:10:57 -0300

    update layout

--- pages/configure-wallet/create.vue
+++ pages/configure-wallet/create.vue
@@ -1,19 +1,63 @@
 <template>
   <ConfigureWalletCard>
-    <div slot="header">
+    <div slot="header" class="text-2xl">
       Set Wallet Password
     </div>
     <div>
-      Choose a wallet password that will be used to unlock this application.
+      <p class="">
+        Choose a wallet password that will be used to unlock this application.
+      </p>
+      <p class="mt-4">
+        While the application is locked, the secret recovery phrase will be encrypted, which prevents sending/receiving funds and managing accounts.
+      </p>
+      <p class="mt-4">
+        For the safety of your funds, use a secure password (the bar should be green).
+      </p>
+
+      <div class="mt-8">
+        <input
+          class="w-full rounded-lg p-2 px-3 dark:bg-gray-900 dark:text-gray-500 border border-gray-900 hover:border-blue-500 outline-none transition-colors"
+          type="password"
+          placeholder="New Wallet Password"
+          v-model="password"
+        >
+        <div class="mx-3" :class="`transition-opacity ${password ? 'opacity-100' : 'opacity-0'}`">
+          <password v-model="password" :strength-meter-only="true" strengthMeterClass="Password__strength-meter mb-0 mt-4"/>
+        </div>
+
+        <input
+          class="w-full mt-4 rounded-lg p-2 px-3 dark:bg-gray-900 dark:text-gray-500 border border-gray-900 hover:border-blue-500 outline-none transition-colors"
+          type="password"
+          placeholder="Confirm Wallet Password"
+          v-model="passwordConfirmation"
+        >
+      </div>
     </div>
     <div slot="footer">
-      <button>next</button>
+      <button
+        :disabled="btnDisabled"
+        :class="`px-4 py-2 w-full sm:w-auto rounded-full  text-white uppercase transition-colors ${btnDisabled ? 'bg-gray-800 text-gray-600' : 'bg-blue-500'}`"
+      >next</button>
     </div>
   </ConfigureWalletCard>
 </template>
 
 <script>
+import Password from 'vue-password-strength-meter'
+
 export default {
   name: 'ConfigureWalletCreate',
+  components: { Password },
+  data() {
+    return {
+      password: null,
+      passwordConfirmation: null,
+    }
+  },
+  computed: {
+    btnDisabled() {
+      return  !this.password || this.password.length < 6 || this.password !== this.passwordConfirmation
+    }
+  }
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  important: true,
   darkMode: 'class',
   content: [
     "./components/**/*.{js,vue,ts}",
